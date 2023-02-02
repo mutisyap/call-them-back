b9a7fd083f6e75d419f14ddf69984ec698f245e9
Peter Mutisya <pmutisya@meliora.tech>
2023-02-02 17:46:34 +0300

    Initialized

---
+++ src/main/java/com/pmutisya/callthemback/CallthembackApplication.java
@@ -0,0 +1,13 @@
+package com.pmutisya.callthemback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallthembackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallthembackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmutisya/callthemback/CallthembackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmutisya.callthemback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallthembackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
